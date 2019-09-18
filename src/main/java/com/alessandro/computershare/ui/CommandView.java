d68222d412e44b7c2bbb15d2738c471aab8f5b45
Alessandro Arosio <alessandro.arosio1@gmail.com>
2019-09-18 08:12:29 +0100

    add getAllDevs

--- src/main/java/com/alessandro/computershare/ui/CommandView.java
+++ src/main/java/com/alessandro/computershare/ui/CommandView.java
@@ -6,6 +6,7 @@ import com.alessandro.computershare.database.service.TaskService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
 import java.util.Scanner;
 
 @Component
@@ -54,6 +55,8 @@ public class CommandView {
         switch (selection) {
           case 1 -> addDeveloper();
           case 2 -> deleteDeveloper();
+          case 3 -> devInfo();
+          case 4 -> getAllDevs();
         }
       } else {
         System.out.printf("Unknown menu\n");
@@ -93,5 +96,35 @@ public class CommandView {
     } catch (Exception e) {
       System.out.println("Dev with id " + devId + " not found. Exiting.");
     }
+  }
+
+  private void devInfo() {
+    System.out.println("=== Developer info ===");
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("Insert ID of the developer you want to see");
+    int devId = scanner.nextInt();
+    try {
+      DeveloperDTO devDTO = developerService.findDevById(devId);
+      System.out.println("Developer's ID: " + devDTO.getId());
+      System.out.println("Developer's name: " + devDTO.getName());
+      System.out.println("Developer's email: " + devDTO.getEmail());
+      System.out.println("\nCurrently working on task(s):\n");
+      devDTO.getTaskList().forEach(e -> {
+        System.out.println("Task ID: " + e.getId());
+        System.out.println("Description: " + e.getDescription());
+        System.out.println("Priority: " + e.getPriority());
+      });
+    } catch (Exception e) {
+      System.out.println("Dev with id " + devId + " not found. Exiting.");
+    }
+  }
+
+  private void getAllDevs() {
+    System.out.println("=== View all developers info ===");
+    List<DeveloperDTO> devList = developerService.getAllDevs();
+    devList.forEach(e -> {
+      System.out.println("Name: " + e.getName() + ", id: " + e.getId() + ", working on " + e.getTaskList().size() + " tasks");
+    });
+    System.out.println("=== END of list");
   }
 }
